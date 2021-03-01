7e452a8f5bf179fe0ebe1f0ec6471137ca61bb69
pablomadriaga <75373441+pablomadriaga@users.noreply.github.com>
2021-03-01 13:31:55 -0300

    v1

---
+++ Export-Tags.ps1
@@ -0,0 +1,29 @@
+$vc = Read-Host -Prompt "Incregre el Vcenter"
+
+#Conectarse al Vcenter
+try {
+    Write-Host "Conectando con vCenter $vcenter , por favor espere ..." -ForegroundColor Cyan
+    Connect-VIServer $vc -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+
+# Crea directorio para las xml de las tags
+$dir_tags = "$env:USERPROFILE\Tags\"
+mkdir $dir_tags
+
+
+
+# Obterner las categorias
+$categoryList = Get-TagCategory -Server $server
+# Obtener las Tags
+$tagList = Get-Tag -Server $server
+# Se crea lista con categorias y tags para exportarlas
+$export = @($categoryList, $tagList)
+# Se realiza export
+Export-Clixml -InputObject $export -Path $dir_tags
+
+
+Disconnect-VIServer * -Force
---
+++ Import-Tags.ps1
@@ -0,0 +1,50 @@
+$vc = Read-Host -Prompt "Incregre el Vcenter"
+
+#Conectarse al Vcenter
+try {
+    Write-Host "Conectando con vCenter $vcenter , por favor espere ..." -ForegroundColor Cyan
+    Connect-VIServer $vc -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+
+# Directorio de las Tags
+$dir_tags = "$env:USERPROFILE\Tags\"
+
+
+# Se realiza el import
+$import = Import-Clixml -Path $dir_tags
+# Se prepara listas de Categorias y Tags
+$categoryList = $import[0]
+$tagList = $import[1]
+   
+# Se crea lista vacia de categorias
+$categories = @()
+
+# Se crean las categorias
+foreach ($category in $categoryList) {
+    $categories += `
+        New-TagCategory `
+        -Name $category.Name `
+        -Description $category.Description `
+        -Cardinality $category.Cardinality `
+        -EntityType $category.EntityType `
+        -Server $server `
+        | Out-Null
+}
+   
+# Se crean las Tags
+foreach ($tag in $tagList) {
+    # Se realiza por categoria
+    $category = $categories | where {$_.Name -eq $tag.Category.Name}
+    if ($category -eq $null) {$category = $tag.Category.Name}
+    
+    New-Tag `
+        -Name $tag.Name `
+        -Description $tag.Description `
+        -Category $category `
+        -Server $server `
+    | Out-Null
+}
